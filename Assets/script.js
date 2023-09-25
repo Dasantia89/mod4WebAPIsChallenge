2074b562d8a5f71ed964234dc9b47f0706e3faf0
Dasantia89 <143134358+Dasantia89@users.noreply.github.com>
2023-09-25 17:30:50 -0400

    Added element references and initialized variables

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,19 @@
 //Assignment Code
 
 var questionAnswer = [
-["Commonly used data types DO NOT include:", "strings","booleans","alerts","numbers",2], 
-["The condition in an if / else statement is enclosed with ___________.", "quotes","curly brackets","parenthesis","square brackets",3],
-[ "Arrays in Javascript can be used to store ________________", "numbers and strings","other arrays","booleans","all of the above",4], 
-["String values must be enclosed within ______ when being assigned to variables.", "commas","curly brackets","quotes","parenthesis",3], 
-[" A very useful tool used during development and debugging for printing content to the debugger is:","JavaScript","terminal/bash","for loops","console.log",4]
+    ["Commonly used data types DO NOT include:", "strings", "booleans", "alerts", "numbers", "alerts"],
+    ["The condition in an if / else statement is enclosed with ___________.", "quotes", "curly brackets", "parenthesis", "square brackets", "parenthesis"],
+    ["Arrays in Javascript can be used to store ________________", "numbers and strings", "other arrays", "booleans", "all of the above", "all of the above"],
+    ["String values must be enclosed within ______ when being assigned to variables.", "commas", "curly brackets", "quotes", "parenthesis", "quotes"],
+    [" A very useful tool used during development and debugging for printing content to the debugger is:", "JavaScript", "terminal/bash", "for loops", "console.log", "console.log"]
 ];
+
+var btn = document.getElementById("start");
+btn.addEventListener("click", startFunction);
+var heading = document.getElementById("heading");
+var questions = document.getElementById("rules");
+var answers = document.querySelectorAll(".answer");
+var timer = document.getElementById("timer");
+var check = document.querySelector("footer");
+var x = 0;
+var time = 75;
--- README.md
+++ README.md
@@ -12,3 +12,4 @@ Code Quiz
 
 ##Citations
 https://www.geeksforgeeks.org/javascript-multidimensional-array/
+https://stackoverflow.com/questions/37594636/how-to-display-message-for-3-seconds-in-javascript
